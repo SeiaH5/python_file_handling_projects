5ce8ad24d91cf6589b56a903eb94beadafa54598
unknown <Josiah Calderon>
2026-04-28 12:49:40 +0800

    Added error handling

--- highest_gwa_identifier/main.py
+++ highest_gwa_identifier/main.py
@@ -8,9 +8,22 @@ class student_record:
     def highest_gwa(self):
         record_file = os.path.join(self.file_path, "student_record.json")
 
-        with open(record_file, "r") as file:
-            record = file.read().strip()
-            students = json.loads(record)
+        try:
+            with open(record_file, "r") as file:
+                record = file.read().strip()
+                students = json.loads(record)
+        
+        except json.JSONDecodeError:
+            print("\n\033[31mJSON file is empty or invalid.\033[0m\n")
+            return
+
+        except FileNotFoundError:
+            print("\n\033[31mFile not found.\033[0m\n")
+            return
+
+        if not students:
+            print("\n\033[31mNo student records found.\033[0m\n")
+            return
 
         highest_name = None
         highest_gwa = float(5.00)
