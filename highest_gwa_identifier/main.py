92bb41a6d071dc39f96964d2fffe4367dd9dab72
unknown <Josiah Calderon>
2026-04-28 12:28:23 +0800

    Added function to load and read the record.

--- highest_gwa_identifier/main.py
+++ highest_gwa_identifier/main.py
@@ -1,8 +1,23 @@
-class student_record:
-    def highest_gwa(self):
-        with open("student_record.json", "r") as file:
-            highest_name = ""
-            highest_gwa = 0
+import os
+import json
 
-        print("Student with highest GWA:")
-        print(highest_name, "-", highest_gwa)
+class student_record:
+    def __init__(self):
+        self.file_path = os.path.dirname(__file__)
+
+    def highest_gwa(self):
+        record_file = os.path.join(self.file_path, "student_record.json")
+
+        with open(record_file, "r") as file:
+            record = file.read().strip()
+
+            if not record:
+                print("Error: JSON file is empty.")
+                return
+
+            students = json.loads(record)
+
+        highest_student = None
+        highest_gwa = float('inf')
+
+student_record().highest_gwa()
