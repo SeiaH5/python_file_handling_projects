46e02a35b53b17ab8f287641a8e5b771cde9240b
unknown <Josiah Calderon>
2026-04-28 12:46:15 +0800

    Displays student with highest gwa

--- highest_gwa_identifier/main.py
+++ highest_gwa_identifier/main.py
@@ -10,14 +10,17 @@ class student_record:
 
         with open(record_file, "r") as file:
             record = file.read().strip()
-
-            if not record:
-                print("Error: JSON file is empty.")
-                return
-
             students = json.loads(record)
 
-        highest_student = None
-        highest_gwa = float('inf')
+        highest_name = None
+        highest_gwa = float(5.00)
+
+        for student in students:
+            if student["gwa"] < highest_gwa:
+                highest_gwa = student["gwa"]
+                highest_name = student["name"]
+
+        print("\n\033[1mStudent with highest GWA:\033[0m")
+        print(highest_name, "-", highest_gwa, "\n")
 
 student_record().highest_gwa()
