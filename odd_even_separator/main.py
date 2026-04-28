464b73d3e1021cd478841189d3805e98c49f6b1c
unknown <Josiah Calderon>
2026-04-28 11:38:32 +0800

    Fixed functions and lines for execution

--- odd_even_separator/main.py
+++ odd_even_separator/main.py
@@ -1,22 +1,19 @@
 
-
-
 class even_odd_identifier:
     def __init__(self):
         self.file_path = "C:\\Users\\User\\Documents\\Coding\\Projects\\simple_file_handling_projects\\odd_even_separator\\" #file directory
         self.even = []
         self.odd = []
 
-    def separate_numbers(self):
-        with open("numbers.txt", "r") as file:
+    def numbers_separator(self):
+        with open(self.file_path + "numbers.txt", "r") as file:
             numbers = file.readlines()
 
-        even_numbers = open("even.txt", "w")
-        odd_numbers = open("odd.txt", "w")
+        even_numbers = open(self.file_path + "even.txt", "w")
+        odd_numbers = open(self.file_path + "odd.txt", "w")
 
         for num in numbers:
             n = int(num.strip())
-
             if n % 2 == 0:
                 even_numbers.write(str(n) + "\n")
             else:
@@ -25,3 +22,5 @@ class even_odd_identifier:
         even_numbers.close()
         odd_numbers.close()
         print("Even and odd numbers saved successfully.")
+
+even_odd_identifier().numbers_separator()
