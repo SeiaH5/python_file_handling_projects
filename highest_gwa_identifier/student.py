327fa750042be495d101384e4586146f9378e0d1
unknown <Josiah Calderon>
2026-04-29 21:03:00 +0800

    Separated class in different file.

---
+++ highest_gwa_identifier/student.py
@@ -0,0 +1,37 @@
+import os
+import json
+
+class student_record:
+    def __init__(self):
+        self.file_path = os.path.dirname(__file__)
+
+    def highest_gwa(self):
+        record_file = os.path.join(self.file_path, "student_record.json")
+
+        try:
+            with open(record_file, "r") as file:
+                record = file.read().strip()
+                students = json.loads(record)
+        
+        except json.JSONDecodeError:
+            print("\n\033[31mJSON file is empty or invalid.\033[0m\n")
+            return
+
+        except FileNotFoundError:
+            print("\n\033[31mFile not found.\033[0m\n")
+            return
+
+        if not students:
+            print("\n\033[31mNo student records found.\033[0m\n")
+            return
+
+        highest_name = None
+        highest_gwa = float(5.00)
+
+        for student in students:
+            if student["gwa"] < highest_gwa:
+                highest_gwa = student["gwa"]
+                highest_name = student["name"]
+
+        print("\n\033[1mStudent with highest GWA:\033[0m")
+        print(highest_name, "-", highest_gwa, "\n")
---
+++ text_content_writer/writer.py
@@ -0,0 +1,26 @@
+import os
+
+class text_writer:
+    def __init__(self):
+        self.file_path = os.path.join(os.path.dirname(__file__), "")
+
+    def write_text(self):
+        file_name = os.path.join(self.file_path, "mylife.txt")
+        print("\n\033[1mMY LIFE TEXT\033[0m")
+
+        with open(file_name, "w") as file:
+            while True:
+                line = input("\nEnter line: ")
+                file.write(line + "\n")
+                
+                while True:
+                    more_input = input("Are there more lines y/n? ").strip().lower()
+                    if more_input in ["y", "n"]:
+                        break
+                    else:
+                        print("\n\033[31mY or N only. Try again\033[0m\n")
+                        continue
+                
+                if more_input == "n":
+                        print("\n\033[1m---END---\033[0m")
+                        break
